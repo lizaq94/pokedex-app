6ac4734d1a6224a79ceef5132cc5a8ad941f6f43
lizaq94 <domagala.k94@gmail.com>
2022-03-30 20:35:57 +0200

    Removes unused state from AppContext

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -2,16 +2,12 @@ import { createContext, useState } from 'react';
 import { Pokemon } from '../types/types';
 
 type AppContextType = {
-  numberOfVisiblePokemons: number;
   pokemons: Pokemon[] | [];
-  setNumberOfVisiblePokemons: (numberOfVisiblePokemons: number) => void;
   setPokemons: (pokemons: (existingPokemons: Pokemon[]) => any[]) => void;
 };
 
 export const AppContext = createContext<AppContextType>({
-  numberOfVisiblePokemons: 0,
   pokemons: [],
-  setNumberOfVisiblePokemons: () => {},
   setPokemons: () => {},
 });
 
@@ -19,15 +15,12 @@ export const AppContextProvider = ({
   children,
 }: JSX.ElementChildrenAttribute) => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  const [numberOfVisiblePokemons, setNumberOfVisiblePokemons] = useState(20);
 
   return (
     <AppContext.Provider
       value={{
-        numberOfVisiblePokemons,
         pokemons,
         setPokemons,
-        setNumberOfVisiblePokemons,
       }}
     >
       {children}
